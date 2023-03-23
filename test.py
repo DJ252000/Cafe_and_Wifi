cf11bf43f1b095b2037bc34d0d2c8056a968de13
Divyajeetsinh <vaghela.conan@gmail.com>
2023-03-24 01:43:00 +0530

    Intial Commit

---
+++ test.py
@@ -0,0 +1,5 @@
+with open("cafe-data.csv", "a", encoding="utf8") as file:
+    filedata = file.read()
+
+print(filedata)
+
---
+++ test1.py
@@ -0,0 +1,8 @@
+fruits = ["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears"]
+vegetables = ["Spinach", "Kale", "Tomatoes", "Celery", "Potatoes"]
+
+dirty_dozen = [fruits, vegetables]
+
+fruit_1 = dirty_dozen[0][1]
+
+print(fruit_1)
